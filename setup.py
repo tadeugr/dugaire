cbcd37dc6062405c18d4f3596eeb0aa9c3a77023
tadeu <tadeugr@gmail.com>
2020-11-19 13:52:55 -0300

    requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = open(os.path.join(HERE, "README.md")).read()
 
 setup(
     name="dugaire",
-    version="0.0.1",
+    version="0.0.3",
     description="Build Docker images with custom packages for local development, testing and daily tasks.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -24,6 +24,6 @@ setup(
     ],
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
-    install_requires=["click", "docker", "jinja2"],
+    install_requires=['certifi==2020.11.8', 'chardet==3.0.4', 'click==7.1.2', 'docker==4.3.1', 'idna==2.10', 'Jinja2==2.11.2', 'MarkupSafe==1.1.1', 'requests==2.25.0', 'six==1.15.0', 'urllib3==1.26.2', 'websocket-client==0.57.0'],
     entry_points={"console_scripts": ["dugaire=dugaire.dugaire:main"]},
 )
