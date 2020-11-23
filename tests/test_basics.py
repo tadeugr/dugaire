81db0d19b8e7631ebf41bfddab27eb977ba46fd4
Tadeu Granemann <tadeu@192.168.0.112>
2020-11-23 19:08:12 -0300

    add tests

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -14,6 +14,7 @@ sys.path.insert(1, f"{HERE}/../dugaire")
 import dugaire
 import info
 
+_BUILT_IMAGES = []
 
 def test_help():
 
@@ -38,3 +39,84 @@ def test_version():
     info_prog_name = info.get_prog_name()
 
     assert f"{info_prog_name}, version {info_version}" == output
+
+def test_build_default():
+    global _BUILT_IMAGES
+
+    cmd = ""
+    cmd += "build"
+
+    result = CliRunner().invoke(dugaire.cli, cmd.split(" "))
+    output = result.output.strip()
+
+    assert 12 == len(output)
+
+    _BUILT_IMAGES.append(output)
+
+def test_build_output_image_id():
+    global _BUILT_IMAGES
+
+    cmd = ""
+    cmd += "build --output=image.id"
+
+    result = CliRunner().invoke(dugaire.cli, cmd.split(" "))
+    output = result.output.strip()
+
+    assert 71 == len(output)
+
+    _BUILT_IMAGES.append(output)
+
+def test_build_output_image_name():
+    global _BUILT_IMAGES
+
+    cmd = ""
+    cmd += "build --output=image.name"
+
+    result = CliRunner().invoke(dugaire.cli, cmd.split(" "))
+    output = result.output.strip()
+
+    assert 21 == len(output)
+
+    _BUILT_IMAGES.append(output)
+
+def test_build_output_dockerfile():
+    global _BUILT_IMAGES
+
+    cmd = ""
+    cmd += "build --output=dockerfile --dry-run"
+
+    result = CliRunner().invoke(dugaire.cli, cmd.split(" "))
+    output = result.output.strip()
+
+    assert "LABEL builtwith" in output
+
+def test_remove_image():
+    global _BUILT_IMAGES
+
+    cmd = ""
+    cmd += f'remove --image={_BUILT_IMAGES[0]}'
+
+    result = CliRunner().invoke(dugaire.cli, cmd.split(" "))
+    output = result.output.strip()
+
+    assert "Image removed." == output
+
+def test_remove_all():
+    global _BUILT_IMAGES
+
+    cmd = ""
+    cmd += f'remove --image=all'
+
+    result = CliRunner().invoke(dugaire.cli, cmd.split(" "))
+    output = result.output.strip()
+
+    assert "Images removed." == output
+
+def test_list_no_image():
+
+    cmd = f"list"
+
+    result = CliRunner().invoke(dugaire.cli, cmd.split(" "))
+    output = result.output.strip()
+
+    assert "No images built with dugaire found." == output
